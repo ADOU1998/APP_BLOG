1a5b9a5d344d2d292f036901e511f587f2854970
ADOU1998 <fabriceadou5@gmail.com>
2022-03-24 13:15:07 +0000

    Création d'une application blog

---
+++ README.md
@@ -0,0 +1,9 @@
+1) Création d'une application web Ruby on rails et est appellé APP BLOG
+
+2) Pour l'excétion du serveur de l'application
+
+3) Les controlleur
+
+4) Les routes
+
+5) Création 
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Page d'accueil
+  root to: 'main#index'
+
+
+  get 'main/about', to: 'main#about'
+end
